206df41da55ecfbbfee09aabb70f0459f128e137
UrwahRafiq <urwahrafiq313@gmail.com>
2026-07-16 16:05:41 +0500

    added testimonial slider and logo carousel

--- app.js
+++ app.js
@@ -26,3 +26,84 @@ document.addEventListener('click', (e) => {
 document.addEventListener('DOMContentLoaded', () => {
     AOS.init();
 });
+
+// testimonial Slider
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.slider-container');
+    const track = document.querySelector('.slider-track');
+    const testimonials = document.querySelectorAll('.testimonial');
+    const dots = document.querySelectorAll('.dot');
+    const prevBtn = document.querySelector('.prev-btn');
+    const nextBtn = document.querySelector('.next-btn');
+
+    //functions for: 1. change active dot color
+    //2. next slide
+    // 3. prev slide
+    // 4. auto play
+    // 5. stop autoplay
+
+    let index = 0;
+    const total = testimonials.length;
+    let autoplay;
+
+    // shifts container to correct slide and updates dot color
+    function updateSlider(){
+        // show slide at current index [0, 1, 2] => [0%, -100%, -200%]
+        track.style.transform = `translateX(-${index * 100}%)`;
+        dots.forEach((dot, i) => {
+            dot.classList.toggle('active', i === index);
+        });
+    }
+
+    function nextSlide(){
+        // proceed to next slide and %total resets index to 0 when at the end, creating a loop
+        index = (index + 1) % total;
+        updateSlider();
+    }
+
+    function prevSlide(){
+        // + total is needed to ensure value stays in the positive
+        index = (index - 1 + total) % total;
+        updateSlider();
+    }
+
+    //execute nextSlide() function every 5s
+    function startAutoplay(){
+        autoplay = setInterval(nextSlide, 2000);
+    }
+
+    // prevents autoplay when user is scrolling
+    function stopAutoplay(){
+        clearInterval(autoplay);
+    }
+
+    // Event listeners for: dots click, button clicks, mouse enter and exit (for autoplay)
+    nextBtn.addEventListener('click', () => {
+        nextSlide();
+        stopAutoplay();
+        startAutoplay();
+    });
+
+    prevBtn.addEventListener('click', () => {
+        prevSlide();
+        stopAutoplay();
+        startAutoplay();
+    });
+
+    // dot's position is identified and index is set to that position
+    dots.forEach((dot, i) => {
+        dot.addEventListener('click', () => {
+            index = i;
+            updateSlider();
+            stopAutoplay();
+            startAutoplay();
+        })
+    });
+
+    const quoteSection = document.querySelector('#quote');
+
+    quoteSection.addEventListener('mouseenter', stopAutoplay);
+    quoteSection.addEventListener('mouseleave', startAutoplay);
+
+    startAutoplay();
+});
